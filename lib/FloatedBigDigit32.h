4f383b4b43153a245ec44a637ac6e5028048bf5a
JPZEBRA <zebra@wing.plala.or.jp>
2021-05-17 18:40:10 +0900

    Update FloatedBigDigit32.h

--- lib/FloatedBigDigit32.h
+++ lib/FloatedBigDigit32.h
@@ -1,7 +1,7 @@
 /* FLOATED BIG DIGIT CLASS */
 /* CREATE  2021.02.06      */
 /* REVISED 2021.05.17      */
-/* Ver 0.5.1               */
+/* Ver 0.6.1               */
 /* Original by K-ARAI      */
 
 
